95c8fc76ad80f84223c789444b4d170566b8d524
avin <esmaili_avin@yahoo.com>
2024-10-24 20:33:51 +0330

    2nd commit

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,8 +7,18 @@ function Calculator() {
   let [hasDot, setHasDot] = useState(false);
   let operators = ["+", "-", "*", "/"];
 
+
+    const checkInput = (text) => {
+    if (text == '÷') return "/";
+    if (text == '×') return "*";
+    return text
+  };
+
+
+
+
   const clickHandler = (e) => {
-    let input = e.target.innerText;
+    let input = checkInput(e.target.innerText);
     // if(input == '.' && result.includes('.')) return
     if (input === ".") {
       if (hasDot === true) return;
@@ -20,14 +30,11 @@ function Calculator() {
     setResult(result + input);
   };
 
-  // const checkInput = (text) => {
-  //   if (text == "÷") return "/";
-  //   if (text == "×") return "*";
-  //   return text
-  // };
+
 
   //   const checkInput = (text)=>{
   //     return text.replace(/&#215;/g, '*').replace(/&#247;/g, '/');
+  //     console.log(checkInput('&#215;')); 
   // }
 
   const clearBtn = () => {
@@ -37,15 +44,13 @@ function Calculator() {
 
   const backSpaceBtn = () => {
     if (result.endsWith(".")) setHasDot(false);
-    else if (result.endsWith("+") && result.includes(".")) setHasDot(false);
     setResult(result.slice(0, -1));
   };
 
   const equalBtn = () => {
     // setResult(eval(result).toString())
     setResult(evaluate(result));
-    if (String(evaluate(result)).includes(".")) setHasDot(true);
-    else setHasDot(false);
+    setHasDot(false);
   };
 
   return (
@@ -59,13 +64,13 @@ function Calculator() {
           C
         </button>
         <button className="color" onClick={clickHandler}>
-          /
+        &#247;
         </button>
         <button onClick={clickHandler}>7</button>
         <button onClick={clickHandler}>8</button>
         <button onClick={clickHandler}>9</button>
         <button className="color" onClick={clickHandler}>
-          *
+        &#215;
         </button>
         <button onClick={clickHandler}>4</button>
         <button onClick={clickHandler}>5</button>
@@ -90,3 +95,77 @@ function Calculator() {
 }
 
 export default Calculator;
+
+// import { useState } from 'react';
+// import './Calculator.css'
+// function Calculator(){
+
+//     let [result , setResult] = useState('')
+//     let [hasDot , setHasDot] = useState(false)
+
+//     let operators = ['-' , '+' , '*' , '/']
+
+//     const checkInput = (text) => {
+//         if (text == '÷') return '/'
+//         if (text == '×') return '*'
+//         return text
+//     }
+
+//     const clickHandler = (e) => {
+//         let input = checkInput(e.target.innerText)
+//         if(input == '.' ){
+//             if(hasDot) return
+//             else setHasDot(true)
+                 
+//         }
+
+//         if(operators.includes(input)){
+//             setHasDot(false)
+//         }
+
+//         setResult(result + input)
+//     }
+
+//     const clearBtn = () => {
+//         setResult('')
+//         setHasDot(false)
+//     }
+
+//     const backSpaceBtn = () => {
+//         if(result.endsWith('.'))
+//             setHasDot(false)
+//         setResult(result.slice(0,-1))
+//     }
+
+//     const equalBtn = () => {
+//         setResult(String(eval(result)))
+//         setHasDot(false)
+//     }
+
+//     return(
+//         <div className="container">
+//             <div className="screen">{result}</div>
+//             <div className="buttons">
+//                 <button onClick={clearBtn} className='color twoCol'>Clear</button>
+//                 <button onClick={backSpaceBtn} className='color'>C</button>
+//                 <button onClick={clickHandler} className='color'>÷</button>
+//                 <button onClick={clickHandler}>7</button>
+//                 <button onClick={clickHandler}>8</button>
+//                 <button onClick={clickHandler}>9</button>
+//                 <button onClick={clickHandler} className='color'>×</button>
+//                 <button onClick={clickHandler}>4</button>
+//                 <button onClick={clickHandler}>5</button>
+//                 <button onClick={clickHandler}>6</button>
+//                 <button onClick={clickHandler} className='color'>-</button>
+//                 <button onClick={clickHandler}>1</button>
+//                 <button onClick={clickHandler}>2</button>
+//                 <button onClick={clickHandler}>3</button>
+//                 <button onClick={clickHandler} className='color'>+</button>
+//                 <button onClick={clickHandler}>0</button>
+//                 <button onClick={clickHandler}>.</button>
+//                 <button onClick={equalBtn} className='color twoCol'>=</button>
+//             </div>
+//         </div>
+//     )
+// }
+// export default Calculator
